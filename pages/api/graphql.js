909ea99d062573bc2105139456d91bc3a9fad2da
todaylab <116174160@qq.com>
2021-06-21 23:25:15 +0800

    add allowCors

--- pages/api/graphql.js
+++ pages/api/graphql.js
@@ -1,5 +1,4 @@
 import { ApolloServer, gql } from 'apollo-server-micro'
-import cors from 'micro-cors' // highlight-line
 
 const topdomains = [
     {
@@ -45,9 +44,21 @@ export const config = {
   },
 }
 
-//export default apolloServer.createHandler({ path: '/api/graphql' })
+const allowCors = fn => async (req, res) => {
+        res.setHeader('Access-Control-Allow-Credentials', true)
+        res.setHeader('Access-Control-Allow-Origin', '*')
+        // another common pattern
+        // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+        res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+        res.setHeader(
+            'Access-Control-Allow-Headers',
+            'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+        )
+        if (req.method === 'OPTIONS') {
+            res.status(200).end()
+            return
+        }
+        return await fn(req, res)
+    }
 
-export default apolloServer.start().then(() => {
-    const handler = apolloServer.createHandler({ path: '/api/graphql' }); // highlight-line
-    return cors((req, res) => req.method === 'OPTIONS' ? res.end() : handler(req, res)) // highlight-line
-});
+export default allowCors(apolloServer.createHandler({ path: '/api/graphql' }))
